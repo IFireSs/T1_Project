4fbe7fcc0eaaa078b9d1e35f36385b6e1bfc0925
Pavel <pasha.varlamow@yandex.ru>
2025-10-22 18:21:00 +0300

    Revert "task 8"
    This reverts commit 83204cde1507f12a8a80b8de278c2059f86e7623.

--- client-processing/src/main/java/com/client_processing/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.client_processing.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {
-}
